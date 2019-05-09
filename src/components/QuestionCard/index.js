5bece6a6992ff915d65b96c4ee6e055e5b12a66d
Turbocake64 <micahrabinowitz@gmail.com>
2019-05-09 13:36:58 -0700

    changing quiz card to class component

--- src/components/QuestionCard/index.js
+++ src/components/QuestionCard/index.js
@@ -2,30 +2,36 @@ import React from "react";
 import "./style.css";
 
 
-function QuestionCard(props) {
-    let { answers } = props;
+// function QuestionCard(props) {
+    class QuestionCard extends React.Component{
 
-    let fakeVar = answers ? answers.splice(answers.indexOf(props[props.category]),1): null
-    let shuffledAnswers = answers ? props.shuffle(answers):null
+    answers  = this.props.answers;
+
+    fakeVar = this.answers ? this.answers.splice(this.answers.indexOf(this.props[this.props.category]),1): null
+    shuffledAnswers = this.answers ? this.props.shuffle(this.answers):null
+    render(){
     return (
         <div className="card">
-        <h2 className="questionName">{props.question}{props.wineName}?</h2>
+        <h2 className="questionName">{this.props.question}{this.props.wineName}?</h2>
                 {/* If false answers are available, render button for each answer, else render a submit (specifically for the flavors question) */}
-                    {answers ? answers.slice(0,3).map(answer => { 
+                    {this.answers ? this.props.answers.slice(0,3).map(answer => { 
                         return (
                         <div>
-                            <button className="false">{ answer }</button><br/>
+                            {this.handleBtnClick}
+                            <button className={this.props.id} value="0">{ answer }</button><br/>
                         </div>
                         )
                     }) : <div>
                             <input></input>
+                            {this.handleBtnClick}
                             <button className="submitAnswer">Submit</button>
                         </div>
                     }
 
-                    {answers ? <button className="true">{props[`${props.category}`]}</button> : null}
+                    {this.answers ? <button className={this.props.id} value="1">{this.props[`${this.props.category}`]}</button> : null}
         </div>
     );
+                }
 }
 
 export default QuestionCard;
--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -16,10 +16,7 @@ class Quiz extends Component {
     questions,
     filteredQs: [],
     wineData,
-    falacy: [],
-    truth: [],
     score: 0,
-    losses: 0,
     highScore: 0,
   };
 
@@ -27,40 +24,18 @@ class Quiz extends Component {
   componentWillMount() {
     const categories = Object.keys(wineData)
     const truth = Object.values(wineData)
-    this.setState({ truth: truth })
-
     const filteredQs = questions.filter(q => {
       return categories.includes(q.category)
     });
+    this.setState({ filteredQs: filteredQs })
+    this.shuffle(filteredQs);
 
     console.log("Categories for", wineData.name, ": ", categories)
     console.log("Answers for ", wineData.name, ": ", truth)
     console.log("Questions to be run for", wineData.name, ": ", filteredQs)
     console.log("The list of false answers for", filteredQs[0].category, ": ", filteredQs[0].falseAnswers)
-
-    this.setState({ filteredQs: filteredQs })
-
-    // let falacy = [];
-    // for (let i = 0; i < filteredQs.length; i++) {
-    //   if ( !truth.includes(filteredQs[i])) {
-    //     falacy.push(filteredQs[i])
-    //   };
-    //   console.timeLog("Falacy: ", falacy)
-    //   return falacy
-    // }
-    
-    // this.setState ({falacy: falacy})
-    // console.log("All false answers: ", falacy)
-    this.shuffle(filteredQs);
   };
-  // filterFalse(arr) {
-  //   for (let i = 0; i < arr.length; i++) {
-  //     if (!truth.includes(arr[i])) {
 
-  //     };
-  //   };
-  //   return arr
-  // };
 
   // Here we use the Fisher-Yates alogrithm to randomize the characters array
   shuffle(arr) {
@@ -74,36 +49,16 @@ class Quiz extends Component {
     return arr;
   };
 
-  handleBtnClick = questionId => {
+  handleBtnClick = () => {
     // renaming this.state so we don't have to write it out each time
-    const powerPuff = { ...this.state };
+    const newState = { ...this.state };
+
+    newState.score = newState.score +1
 
     // in this if statement we check to see if the player lost, log their loss, and clear the guesses array & score
-    if (powerPuff.guesses.includes(questionId)) {
-      console.log("You lose");
-      powerPuff.losses = powerPuff.losses + 1;
-      powerPuff.guesses = [];
-      powerPuff.score = 0
-    }
-
-    if ("button.true") {
-      powerPuff.score = powerPuff.score + 1;
-    }
-    // if they didn't lose, we increment their score and add the character to the guesses array
-    // else
-    // { powerPuff.score = powerPuff.score +1;
-    //   powerPuff.guesses.push(questionId)
-    // }
-    // here we shuffle the character cards again
-    this.shuffle(questions);
-
-    // we check the current score against the high score, increasing high score if it is lower than current score.
-    if (powerPuff.score > powerPuff.highScore) {
-      powerPuff.highScore = powerPuff.score
-    }
 
     // finally we use setState to update the state to the virtual DOM
-    this.setState(powerPuff);
+    this.setState(newState);
   }
 
 
@@ -120,7 +75,6 @@ class Quiz extends Component {
               // each card will inherit an id, a key, a name, and an image from its respective array object
               handleBtnClick={this.handleBtnClick}
               shuffle={this.shuffle}
-              filterFalse={this.filterFalse}
 
               id={filteredQ.id}
               key={filteredQ.id}
