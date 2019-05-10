f094c69fd2eeb042b20d0476f70188b5ba6a5b52
Leif <leifaesoph@gmail.com>
2019-05-10 14:48:59 -0700

    Updates

---
+++ precache-manifest.8e5c056a47b43cd0b3df606ec3350bf1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dafb2ad3982900ac4088",
+    "url": "/Tannin/static/js/runtime~main.b71e23b5.js"
+  },
+  {
+    "revision": "09372db66d054f495b65",
+    "url": "/Tannin/static/js/main.edd15e0a.chunk.js"
+  },
+  {
+    "revision": "ffa17390fd4ed2169991",
+    "url": "/Tannin/static/js/2.9e060e50.chunk.js"
+  },
+  {
+    "revision": "09372db66d054f495b65",
+    "url": "/Tannin/static/css/main.81311cb3.chunk.css"
+  },
+  {
+    "revision": "903ea3c76b0d91dd2a4970e87f12f450",
+    "url": "/Tannin/index.html"
+  }
+];
