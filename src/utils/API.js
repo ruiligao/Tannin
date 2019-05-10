33062cf899b63898f24057780be239c90a58f0d5
Leif <leifaesoph@gmail.com>
2019-05-10 14:26:31 -0700

    pb2

--- src/utils/API.js
+++ src/utils/API.js
@@ -50,50 +50,50 @@ export default {
 
 //LOCAL ROUTES
 //------------------------------------------------------
-signUpSubmit: function (userInfo) {
-  console.log(userInfo);
-  return axios.post("/api/user/signup", userInfo);
-},
-logIn: function (loginInfor) {
-  console.log(loginInfor);
-  return axios.post("/api/user/login", loginInfor);
-},
-logOut: function () {
-  // console.log(userInfo);
-  return axios.post("/api/user/logout");
-},
-getUser: function () {
-  return axios.get('/api/user/getUser');
-},
-  getMaster: function(q) {
-    return axios.get("http://localhost:3001/api/wine/");
-  },
+// signUpSubmit: function (userInfo) {
+//   console.log(userInfo);
+//   return axios.post("/api/user/signup", userInfo);
+// },
+// logIn: function (loginInfor) {
+//   console.log(loginInfor);
+//   return axios.post("/api/user/login", loginInfor);
+// },
+// logOut: function () {
+//   // console.log(userInfo);
+//   return axios.post("/api/user/logout");
+// },
+// getUser: function () {
+//   return axios.get('/api/user/getUser');
+// },
+//   getMaster: function(q) {
+//     return axios.get("http://localhost:3001/api/wine/");
+//   },
 
-  getSavedWine: function(admin) {
-    console.log(admin);
-    return axios.post("http://localhost:3001/api/getwine/",admin);
-  },
-  addEmployee: function(employeeData) {
-    console.log(employeeData);
-    return axios.post("/api/addEmployee", employeeData)
-  },
-  addWine: function(wineData) {
-    console.log(wineData);
-    return axios.put("http://localhost:3001/api/addwine/", wineData);
-  },
-  // getRestoId: function(wineData) {
-  //   console.log(wineData);
-  //   return axios.put("http://localhost:3001/api/addwine/", wineData);
-  // },
+//   getSavedWine: function(admin) {
+//     console.log(admin);
+//     return axios.post("http://localhost:3001/api/getwine/",admin);
+//   },
+//   addEmployee: function(employeeData) {
+//     console.log(employeeData);
+//     return axios.post("/api/addEmployee", employeeData)
+//   },
+//   addWine: function(wineData) {
+//     console.log(wineData);
+//     return axios.put("http://localhost:3001/api/addwine/", wineData);
+//   },
+//   // getRestoId: function(wineData) {
+//   //   console.log(wineData);
+//   //   return axios.put("http://localhost:3001/api/addwine/", wineData);
+//   // },
   
-  deleteWine: function(delelteWine) {
-    console.log(delelteWine);
-    return axios.put("http://localhost:3001/api/restaurant/delete", delelteWine);
-  },
-  deleteEmployee: function(deleteEmp) {//localhost:3001/api/addEmployee/
-    console.log(deleteEmp);
-    return axios.put("http://localhost:3001/api/addEmployee/", deleteEmp)
-  }
+//   deleteWine: function(delelteWine) {
+//     console.log(delelteWine);
+//     return axios.put("http://localhost:3001/api/restaurant/delete", delelteWine);
+//   },
+//   deleteEmployee: function(deleteEmp) {//localhost:3001/api/addEmployee/
+//     console.log(deleteEmp);
+//     return axios.put("http://localhost:3001/api/addEmployee/", deleteEmp)
+//   }
 
 
 
