74ca99ee1ee36c3f14035c690576ba4924a709c3
Dennis <dennissarmiento808@gmail.com>
2019-05-11 20:37:39 -0700

    up

--- src/components/QuestionCard/index.js
+++ src/components/QuestionCard/index.js
@@ -11,11 +11,52 @@ class QuestionCard extends React.Component {
     dummThing = this.selections ? (this.selections.push(this.props[`${this.props.category}`])) : null;
     // shuffledSelections = this.selections ? this.props.shuffle(this.selections) : null;
 
+
+    constructor(props) {
+        super(props);
+          this.state = {
+            bgColor: [
+                '#f89406',
+'#16a085',
+'#68c3a3',
+'#89c4f4',
+
+
+              '#d5b8ff',
+              '#a2ded0',
+              '#19b5fe',
+              '#f0f0d6',
+              '#f4f776',
+              '#f2784b',
+              '#67809f',
+              '#95a5a6',
+              '#f1a9a0',
+              '#be90d4',
+              '#22a7f0',
+              '#c8f7c5',
+            ],
+            selectedColor: '',
+          };
+        }
+        
+        
+        componentDidMount() {
+          this._getRandomColor()
+        }
+        
+        _getRandomColor(){
+          var item = this.state.bgColor[Math.floor(Math.random()*this.state.bgColor.length)];
+          this.setState({
+            selectedColor: item,
+          })
+        }
+        
+
     render() {
         console.log("selections");
         console.log(this.selections);
         return (
-            <div className="qcard">
+            <div className="qcard" style={{backgroundColor: this.state.selectedColor}}>
             <div className="qcard2">
 
                 <h2 className="questionName">{this.props.question}{this.props.wineName}?</h2>
