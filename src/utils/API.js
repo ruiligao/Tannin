19ea51d6f7f323a06d07994b4175057835942b3e
ruiligao <ruiligao@gmail.com>
2019-05-29 00:13:52 -0700

    rg

--- src/utils/API.js
+++ src/utils/API.js
@@ -5,47 +5,50 @@ export default {
 
   //HEROKU ROUTES
 
-  // signUpSubmit: function (userInfo) {
-  //   console.log(userInfo);
-  //   return axios.post("https://tannin-api.herokuapp.com/api/user/signup", userInfo);
-  // },
-  // logIn: function (loginInfor) {
-  //   console.log(loginInfor);
-  //   return axios.post("https://tannin-api.herokuapp.com/api/user/login", loginInfor);
-  // },
-  // logOut: function () {
-  //   // console.log(userInfo);
-  //   return axios.post("https://tannin-api.herokuapp.com/api/user/logout");
-  // },
-  // getUser: function () {
-  //   return axios.get('https://tannin-api.herokuapp.com/api/user/getUser');
-  // },
-  //   getMaster: function(q) {
-  //     return axios.get("https://tannin-api.herokuapp.com/api/wine/");
-  //   },
+  signUpSubmit: function (userInfo) {
+    console.log(userInfo);
+    return axios.post("https://tanninapi.herokuapp.com/api/user/signup", userInfo);
+  },
+  logIn: function (loginInfor) {
+    console.log(loginInfor);
+    return axios.post("https://tanninapi.herokuapp.com/api/user/login", loginInfor);
+  },
+  logOut: function () {
+    // console.log(userInfo);
+    return axios.post("https://tanninapi.herokuapp.com/api/user/logout");
+  },
+  getUser: function () {
+    return axios.get('https://tanninapi.herokuapp.com/api/user/getUser');
+  },
+    getMaster: function(q) {
+      return axios.get("https://tanninapi.herokuapp.com/api/wine/");
+    },
 
-  //   getSavedWine: function(admin) {
-  //     console.log(admin);
-  //     return axios.post("https://tannin-api.herokuapp.com/api/getwine/",admin);
-  //   },
-  //   addEmployee: function(employeeData) {
-  //     console.log(employeeData);
-  //     return axios.post("https://tannin-api.herokuapp.com/api/addEmployee", employeeData)
-  //   },
-  //   addWine: function(wineData) {
-  //     console.log(wineData);
-  //     return axios.put("https://tannin-api.herokuapp.com/api/addwine/", wineData);
-  //   },
+    getSavedWine: function(admin) {
+      console.log(admin);
+      return axios.post("https://tanninapi.herokuapp.com/api/getwine/",admin);
+    },
+    addEmployee: function(employeeData) {
+      console.log(employeeData);
+      return axios.post("https://tanninapi.herokuapp.com/api/addEmployee", employeeData)
+    },
+    addWine: function(wineData) {
+      console.log(wineData);
+      return axios.put("https://tanninapi.herokuapp.com/api/addwine/", wineData);
+    },
 
-  //   deleteWine: function(delelteWine) {
-  //     console.log(delelteWine);
-  //     return axios.put("https://tannin-api.herokuapp.com/api/restaurant/delete", delelteWine);
-  //   },
-  //   deleteEmployee: function(deleteEmp) {//localhost:3001/api/addEmployee/
-  //     console.log(deleteEmp);
-  //     return axios.put("https://tannin-api.herokuapp.com/api/addEmployee/", deleteEmp)
-  //   },
-
+    deleteWine: function(delelteWine) {
+      console.log(delelteWine);
+      return axios.put("https://tanninapi.herokuapp.com/api/restaurant/delete", delelteWine);
+    },
+    deleteEmployee: function(deleteEmp) {//localhost:3001/api/addEmployee/
+      console.log(deleteEmp);
+      return axios.put("https://tanninapi.herokuapp.com/api/addEmployee/", deleteEmp)
+    },
+  addScore: function (scoreData) {//localhost:3001/api/addEmployee/
+    console.log(scoreData);
+    return axios.put("https://tanninapi.herokuapp.com/api/employees/score", scoreData)
+  },
 
 
   //LOCAL ROUTES
